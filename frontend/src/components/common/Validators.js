cc88ca4b5860aff3c402bdc2174d88218725c425
Rick00Kim <dreamx119@gmail.com>
2022-01-10 23:43:13 +0900

    Add validate to Signup form

---
+++ frontend/src/components/common/Validators.js
@@ -0,0 +1,65 @@
+export const requireValidation = (target) => {
+  if (typeof target === "number") {
+    return !target;
+  }
+  if (typeof target === "string") {
+    return target.trim() === "";
+  }
+};
+
+export const nameValidation = (fieldValue) => {
+  if (requireValidation(fieldValue)) {
+    return `name is required`;
+  }
+  if (/[^a-zA-Z -]/.test(fieldValue)) {
+    return "Invalid characters";
+  }
+  if (fieldValue.trim().length < 3) {
+    return `name needs to be at least three characters`;
+  }
+  return null;
+};
+
+export const passwordValidation = (fieldValue) => {
+  if (requireValidation(fieldValue)) {
+    return `password is required`;
+  }
+  if (fieldValue.trim().length < 8) {
+    return `password needs to be at least 8 characters`;
+  }
+  if (
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/.test(
+      fieldValue
+    )
+  ) {
+    return "Invalid characters";
+  }
+  return null;
+};
+
+export const emailValidation = (email) => {
+  if (
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    return null;
+  }
+  if (requireValidation(email)) {
+    return "Email is required";
+  }
+  return "Please enter a valid email";
+};
+
+export const ageValidation = (age) => {
+  if (requireValidation(age)) {
+    return "Age is required";
+  }
+  if (age < 18) {
+    return "Age must be at least 18";
+  }
+  if (age > 99) {
+    return "Age must be under 99";
+  }
+  return null;
+};
--- frontend/src/components/sign/SignUpForm.js
+++ frontend/src/components/sign/SignUpForm.js
@@ -4,6 +4,11 @@ import axios from "axios";
 import { Form, Button } from "react-bootstrap";
 import { backendPointList, routerEndPoint } from "../common/Constants";
 import "bootstrap/dist/css/bootstrap.css";
+import {
+  emailValidation,
+  nameValidation,
+  passwordValidation,
+} from "../common/Validators";
 
 const componentStyle = {
   root: {
@@ -17,6 +22,12 @@ const componentStyle = {
   },
 };
 
+const validate = {
+  email: (v) => emailValidation(v),
+  password: (v) => passwordValidation(v),
+  name: (v) => nameValidation(v),
+};
+
 function SignUpForm() {
   const navigate = useNavigate();
   const [form, setForm] = useState({
@@ -24,24 +35,75 @@ function SignUpForm() {
     password: "",
     name: "",
   });
+  const [touched, setTouched] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handleInput = (props) => {
     const { name, value } = props.target;
-    setForm({ ...form, [name]: value });
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
+  const handleBlur = (props) => {
+    const { name, value } = props.target;
+    const { [name]: removedError, ...rest } = errors;
+    const error = validate[name](value);
+    setErrors({
+      ...rest,
+      ...(error && { [name]: touched[name] && error }),
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .post(backendPointList.signup, form)
-      .then((res) => {
-        navigate(routerEndPoint.root);
-      })
-      .catch((err) => console.log(err));
+
+    const formValidation = Object.keys(form).reduce(
+      (acc, key) => {
+        const newError = validate[key](form[key]);
+        const newTouched = { [key]: true };
+        return {
+          errors: {
+            ...acc.errors,
+            ...(newError && { [key]: newError }),
+          },
+          touched: {
+            ...acc.touched,
+            ...newTouched,
+          },
+        };
+      },
+      {
+        errors: { ...errors },
+        touched: { ...touched },
+      }
+    );
+
+    setErrors(formValidation.errors);
+    setTouched(formValidation.touched);
+
+    if (
+      !Object.values(formValidation.errors).length &&
+      Object.values(formValidation.touched).length ===
+        Object.values(form).length &&
+      Object.values(formValidation.touched).every((t) => t === true)
+    ) {
+      axios
+        .post(backendPointList.signup, form)
+        .then((res) => {
+          navigate(routerEndPoint.root);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
-    <Form style={componentStyle.root}>
+    <Form style={componentStyle.root} autoComplete="off">
       <Form.Group controlId="formEmail" style={componentStyle.formGroupStyle}>
         <Form.Label>Email</Form.Label>
         <Form.Control
@@ -50,7 +112,13 @@ function SignUpForm() {
           name="email"
           value={form.email}
           onChange={(e) => handleInput(e)}
+          onBlur={(e) => handleBlur(e)}
+          required
+          isInvalid={errors.email}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.email}
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group
         controlId="formPassword"
@@ -63,8 +131,13 @@ function SignUpForm() {
           name="password"
           value={form.password}
           onChange={(e) => handleInput(e)}
+          onBlur={(e) => handleBlur(e)}
           autoComplete="off"
+          isInvalid={errors.password}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.password}
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formName" style={componentStyle.formGroupStyle}>
         <Form.Label>Name</Form.Label>
@@ -74,7 +147,12 @@ function SignUpForm() {
           name="name"
           value={form.name}
           onChange={(e) => handleInput(e)}
+          onBlur={(e) => handleBlur(e)}
+          isInvalid={errors.name}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.name}
+        </Form.Control.Feedback>
       </Form.Group>
       <Button
         variant="primary"
