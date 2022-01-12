e50613605c7bdc507d72e9356d766c014ca9f394
Rick00Kim <dreamx119@gmail.com>
2022-01-12 23:42:09 +0900

    Modify AuthService [Get Current user]

--- frontend/src/components/authenticated/AuthService.js
+++ frontend/src/components/authenticated/AuthService.js
@@ -1,7 +1,10 @@
 import jwtDecode from "jwt-decode";
 
 export const getCurrentUser = () => {
+  let decoded = "";
   const auth_info = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
-  const decoded = jwtDecode(auth_info)["sub"];
+  if (auth_info != null) {
+    decoded = jwtDecode(auth_info)["sub"];
+  }
   return decoded;
 };
