096caf83cc6d234ab6c0c9bdcb42ebb9a4a8e70a
Rick00Kim <dreamx119@gmail.com>
2022-01-15 06:11:26 +0900

    Modify backend [modulation]

---
+++ backend/module/contoller/like.py
@@ -0,0 +1,63 @@
+import json
+from flask import Response, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from .. import app
+from ..model.dto import *
+
+
+@app.route("/api/like/<int:debate_num>", methods=['GET'])
+@jwt_required()
+def get_debate_like(debate_num):
+    current_user = get_jwt_identity()
+
+    user = UserInfo.objects(email=current_user['email']).first()
+
+    response = {
+        'like_cnt': LikeOnDebate.objects(debate_num=debate_num).count(),
+        'unlike_cnt': UnLikeOnDebate.objects(debate_num=debate_num).count(),
+        'liked': False if LikeOnDebate.objects(debate_num=debate_num, user_id=user['id']).count() == 0 else True,
+        'unliked': False if UnLikeOnDebate.objects(debate_num=debate_num, user_id=user['id']).count() == 0 else True
+    }
+
+    return Response(json.dumps(response), mimetype="application/json", status=200)
+
+
+@app.route("/api/like", methods=['POST'])
+@jwt_required()
+def post_debate_like():
+    task = request.json
+    current_user = get_jwt_identity()
+
+    user = UserInfo.objects(email=current_user['email']).first()
+
+    if LikeOnDebate.objects(debate_num=task['debate_id'], user_id=user['id']).count() == 0:
+        LikeOnDebate(debate_num=task['debate_id'], user_id=user['id']).save()
+        if UnLikeOnDebate.objects(debate_num=task['debate_id'], user_id=user['id']).count() > 0:
+            UnLikeOnDebate.objects(
+                debate_num=task['debate_id'], user_id=user['id']).delete()
+    else:
+        LikeOnDebate.objects(
+            debate_num=task['debate_id'], user_id=user['id']).delete()
+
+    return Response("SUCCESS", mimetype="application/json", status=200)
+
+
+@app.route("/api/unlike", methods=['POST'])
+@jwt_required()
+def post_debate_unlike():
+    task = request.json
+    current_user = get_jwt_identity()
+
+    user = UserInfo.objects(email=current_user['email']).first()
+
+    if UnLikeOnDebate.objects(debate_num=task['debate_id'], user_id=user['id']).count() == 0:
+        UnLikeOnDebate(
+            debate_num=task['debate_id'], user_id=user['id']).save()
+        if LikeOnDebate.objects(debate_num=task['debate_id'], user_id=user['id']).count() > 0:
+            LikeOnDebate.objects(
+                debate_num=task['debate_id'], user_id=user['id']).delete()
+    else:
+        UnLikeOnDebate.objects(debate_num=task['debate_id'],
+                               user_id=user['id']).delete()
+
+    return Response("SUCCESS", mimetype="application/json", status=200)
---
+++ backend/run.py
@@ -0,0 +1,4 @@
+from module import *
+
+if __name__ == "__main__":
+    flask_app.run(debug=True, port=5500)
