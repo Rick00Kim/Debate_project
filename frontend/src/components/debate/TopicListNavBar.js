2d29ceedcc6bfe574f65a9120cc2f843d6f605a4
Rick00Kim <dreamx119@gmail.com>
2022-03-16 22:54:44 +0900

    Modify Topic Navbar

--- frontend/src/components/debate/TopicListNavBar.js
+++ frontend/src/components/debate/TopicListNavBar.js
@@ -1,128 +1,92 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Jumbotron, Container, Row, ListGroup, Button } from "react-bootstrap";
-import { contentStyle, routerEndPoint } from "../common/Constants";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { routerEndPoint } from "../common/Constants";
 import { useAuth, logout } from "../authenticated/auth";
 import { getCurrentUser } from "../authenticated/AuthService";
 import LogoutButton from "../sign/SignOut";
-import ManagerComponet from "../management/ManagerComponent";
+import ManagerComponent from "../management/ManagerComponent";
 import "bootstrap/dist/css/bootstrap.css";
-
-const componentStyle = {
-  root: contentStyle.root,
-  listGroupItemStyle: {
-    padding: `0.3rem 1rem`,
-  },
-  rowStyle: {
-    justifyContent: "center",
-  },
-  headerStyle: {
-    paddingBottom: 20,
-    margin: 0,
-  },
-  rootLinkStyle: {
-    color: "#fdfdfe",
-    textDecoration: "none",
-  },
-  newBtnStyle: {
-    position: "absolute",
-    left: "5%",
-  },
-  usersStyle: {
-    position: "absolute",
-    right: "10%",
-    bottom: "11%",
-  },
-  topicListStyle: {
-    height: `70vh`,
-    maxHeight: `70vh`,
-    fontSize: `20px`,
-    borderRadius: `5px`,
-    overflow: `scroll`,
-  },
-  topicElementStyle: {
-    textDecoration: "none",
-  },
-  userInfoStyle: {
-    height: `17%`,
-    marginTop: 10,
-    paddingTop: `2rem`,
-    marginBottom: 10,
-    padding: `20px 0`,
-    color: `white`,
-    textAlign: `left`,
-    backgroundColor: "inherit",
-  },
-};
 
 const TopicListNavBar = (props) => {
   const [logged] = useAuth();
-  const { topicList, currentTopic, setCurrentTopic } = props;
+  const { topicList, setCurrentTopic } = props;
+  const [expanded, setExpanded] = useState(false);
 
-  const UserInfo = () => {
-    return logged ? (
-      <Container>
-        <ManagerComponet
-          render={(props) => (
-            <Link
-              to={routerEndPoint.manager.users}
-              style={componentStyle.usersStyle}
-              {...props}
-            >
-              <Button variant="outline-warning">Users</Button>
-            </Link>
-          )}
-        />
-        <h3>{getCurrentUser().name}</h3>
-        <h4>Role: {getCurrentUser().role}</h4>
-        <LogoutButton logout={logout} />
-      </Container>
-    ) : (
-      ""
-    );
+  const dropdownClickHandler = (e) => {
+    setCurrentTopic(e);
+    setExpanded(!expanded);
   };
 
   return (
-    <div style={componentStyle.root}>
-      <Row style={componentStyle.rowStyle}>
-        <Link to={routerEndPoint.root} style={componentStyle.rootLinkStyle}>
-          <h3 style={componentStyle.headerStyle}>Choose Topic</h3>
-        </Link>
-        <ManagerComponet
-          render={(props) => (
-            <Link
-              to={routerEndPoint.addTopic}
-              style={componentStyle.newBtnStyle}
-            >
-              <Button variant="light">New</Button>
-            </Link>
-          )}
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        expanded={expanded}
+      >
+        <Navbar.Brand as={NavLink} to={routerEndPoint.root}>
+          KURURU DEBATE
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={(e) => setExpanded(!expanded)}
         />
-      </Row>
-      <ListGroup style={componentStyle.topicListStyle}>
-        {topicList.map((e) => (
-          <Link
-            to={routerEndPoint.debates + e._id}
-            style={componentStyle.topicElementStyle}
-            key={`topicKey-${e._id}`}
-          >
-            <ListGroup.Item
-              style={componentStyle.listGroupItemStyle}
-              action
-              active={currentTopic._id === e._id}
-              variant="light"
-              as="div"
-              key={"topic" + e._id}
-              onClick={(i) => setCurrentTopic(e)}
-            >
-              {e.title}
-            </ListGroup.Item>
-          </Link>
-        ))}
-      </ListGroup>
-      <Jumbotron fluid style={componentStyle.userInfoStyle}>
-        <UserInfo />
-      </Jumbotron>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {logged ? (
+            <h4>
+              {getCurrentUser().name} ({getCurrentUser().role})
+            </h4>
+          ) : (
+            ""
+          )}
+          <Nav className="mr-auto">
+            <NavDropdown title="Choose Topic" id="collasible-nav-dropdown">
+              {topicList.map((e) => (
+                <NavDropdown.Item
+                  as={NavLink}
+                  to={routerEndPoint.debates + e._id}
+                  onClick={(i) => dropdownClickHandler(e)}
+                >
+                  {e.title}
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
+          </Nav>
+          {logged ? (
+            <Nav>
+              <ManagerComponent
+                render={(props) => (
+                  <Nav.Link
+                    as={NavLink}
+                    to={routerEndPoint.addTopic}
+                    onClick={(e) => setExpanded(!expanded)}
+                  >
+                    New Topic
+                  </Nav.Link>
+                )}
+              />
+              <ManagerComponent
+                render={(props) => (
+                  <Nav.Link
+                    as={NavLink}
+                    to={routerEndPoint.manager.users}
+                    onClick={(e) => setExpanded(!expanded)}
+                  >
+                    Users
+                  </Nav.Link>
+                )}
+              />
+              <LogoutButton logout={logout} />
+            </Nav>
+          ) : (
+            ""
+          )}
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
