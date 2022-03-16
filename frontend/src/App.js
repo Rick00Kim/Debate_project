749b4b7a437cd1eb3c367ef78c2e13f03bc6223f
Rick00Kim <dreamx119@gmail.com>
2022-03-16 22:55:11 +0900

    Add controller SP view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,10 @@ import {
   Route,
   Routes,
   Navigate,
-  NavLink,
 } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
-import { Nav, Navbar } from "react-bootstrap";
 import TopicListBar from "./components/debate/TopicListBar";
+import TopicListNavBar from "./components/debate/TopicListNavBar";
 import TopicContent from "./components/debate/TopicContent";
 import { backendPointList } from "./components/common/Constants";
 import SignIn from "./components/sign/SignIn";
@@ -98,34 +97,18 @@ function App() {
     );
   };
 
-  const NavBar = () => {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">Dice Roller</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={NavLink} to="features">
-              features
-            </Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  };
-
   return (
     <div className="App-header">
       <div style={componentStyle.mainStyle}>
         <Router>
-          {responsiveMobile.showTopNavMenu ? NavBar() : ""}
+          {responsiveMobile.showTopNavMenu ? (
+            <TopicListNavBar
+              topicList={topicList}
+              setCurrentTopic={setCurrentTopic}
+            />
+          ) : (
+            ""
+          )}
           <Row style={componentStyle.contentRowStyle}>
             {responsiveMobile.showTopNavMenu ? (
               ""
